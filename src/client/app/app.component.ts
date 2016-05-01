4894dc164c8169092d158d39fe36a3eef6174516
Michael Westphal <michael@michaelwestphal.org>
2016-05-01 14:52:08 -0500

    Finishing part 1 of the tutorial.
    Added the basic Hero editor component with one-way and two-way (via
    ngModel) data binding.
    
    Also deleted unused css and html file generated by the angular-cli.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,10 +1,27 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
-//   moduleId: __moduleName,
+  //   moduleId: __moduleName,
   selector: 'my-app',
-//   templateUrl: 'app.component.html',
-//   styleUrls: ['app.component.css']
-  template: '<h1>My first Angular 2 app</h1>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
