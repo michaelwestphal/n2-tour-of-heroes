f20f2badf2eca691405886190f668adddaf59028
Michael Westphal <michael@michaelwestphal.org>
2016-05-03 06:53:15 -0500

    Fixing the App component spec after Angular beta.17 upgrade.

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -1,23 +1,22 @@
 import {
   beforeEachProviders,
   describe,
-  ddescribe,
-  expect,
-  iit,
+  // ddescribe,
+  // expect,
+  // iit,
   it,
   inject,
-  injectAsync,
   ComponentFixture,
   TestComponentBuilder
 } from 'angular2/testing';
-import {provide} from 'angular2/core';
+// import {provide} from 'angular2/core';
 import {AppComponent} from './app.component';
 
 describe('MyApp Component', () => {
 
   beforeEachProviders((): any[] => []);
 
-  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+  it('should ...', inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
     return tcb.createAsync(AppComponent).then((fixture: ComponentFixture) => {
       fixture.detectChanges();
     });
