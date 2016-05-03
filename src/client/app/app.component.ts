dca9351af09eea23bef4b7e1b12089f25f2f6a93
Michael Westphal <michael@michaelwestphal.org>
2016-05-03 07:20:05 -0500

    First steps to set up the App component to use the HeroService.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @Component({
   //   moduleId: __moduleName,
@@ -67,27 +68,17 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private _heroService: HeroService) {}
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
 }
-
-var HEROES: Hero[] = [
-  {'id': 11, 'name': 'Mr. Nice'},
-  {'id': 12, 'name': 'Narco'},
-  {'id': 13, 'name': 'Bombasto'},
-  {'id': 14, 'name': 'Celeritas'},
-  {'id': 15, 'name': 'Magneta'},
-  {'id': 16, 'name': 'RubberMan'},
-  {'id': 17, 'name': 'Dynama'},
-  {'id': 18, 'name': 'Dr IQ'},
-  {'id': 19, 'name': 'Magma'},
-  {'id': 20, 'name': 'Tornado'}
-];
