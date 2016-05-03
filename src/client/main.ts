8bd3559900b161b89a6329eea0327d1eff1be4c0
Michael Westphal <michael@michaelwestphal.org>
2016-05-03 06:53:40 -0500

    Removing uncessary whitespace within main.ts import.

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,7 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {enableProdMode} from 'angular2/core';
 import {environment} from './app/environment';
-import { AppComponent } from './app/app.component';
+import {AppComponent} from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
