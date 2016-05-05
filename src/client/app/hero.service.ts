349c7b7a60dedd647799da34b5c1ccf57d1081d8
Michael Westphal <michael@michaelwestphal.org>
2016-05-05 07:18:51 -0500

    Promises and getHeroes function on HeroService.
    I also added optional typings to the function and place a fake 2 second timeout to simulate a real API connection before resolving the Promise.

--- src/client/app/hero.service.ts
+++ src/client/app/hero.service.ts
@@ -1,11 +1,13 @@
 import {Injectable} from 'angular2/core';
-// import {Hero} from './hero';
+import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-  // getHeros(): Hero[] {
-  getHeroes() {
-    return HEROES;
+  getHeroes(): Promise<Hero[]> {
+    // return Promise.resolve(HEROES);
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 2000)
+    );
   }
 }
