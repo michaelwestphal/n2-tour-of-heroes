c810d414860c336cde54534558fa84690d6033ca
Michael Westphal <michael@michaelwestphal.org>
2016-05-10 07:15:13 -0500

    Updating the Hero Detail component to be routeable.
    Changing the component to set its initial state on init by getting the Hero ID from the route parameters.
    
    Externalizing the template as well.

--- src/client/app/hero-detail.component.ts
+++ src/client/app/hero-detail.component.ts
@@ -1,20 +1,29 @@
-import { Component, Input } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { RouteParams } from 'angular2/router';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: __moduleName,
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'hero-detail.component.html'
 })
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit {
+  // @Input()
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private routeParams: RouteParams) { }
+
+  ngOnInit() {
+    let id = +this.routeParams.get('id');
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
 }
