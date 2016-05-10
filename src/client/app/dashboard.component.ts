aa57b8c6d3034b98d59978ddf1aaed8d9502d982
Michael Westphal <michael@michaelwestphal.org>
2016-05-10 07:15:41 -0500

    Adding a Dashboard component and template.

---
+++ src/client/app/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  moduleId: __moduleName,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', { id: hero.id }];
+    this.router.navigate(link);
+  }
+}
