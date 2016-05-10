8c5758182b6a3705acd390fba8c6497c6a1d064b
Michael Westphal <michael@michaelwestphal.org>
2016-05-10 07:16:23 -0500

    Adding new routes to AppComponent.
    Adding a new route to the Dashboard and HeroDetail components.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
@@ -9,7 +11,10 @@ import { HeroService } from './hero.service';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['Heroes']">Heroes</a>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
@@ -17,9 +22,20 @@ import { HeroService } from './hero.service';
 })
 @RouteConfig([
   {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 export class AppComponent {
