70c0585315be065d4728cf762fcfe617e42167b5
Michael Westphal <michael@michaelwestphal.org>
2016-05-12 07:13:04 -0500

    Removing built-in fake latency from the HeroService.

--- src/client/app/hero.service.ts
+++ src/client/app/hero.service.ts
@@ -5,18 +5,10 @@ import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
   getHeroes(): Promise<Hero[]> {
-    // return Promise.resolve(HEROES);
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 2000)
-    );
+    return Promise.resolve(HEROES);
   }
 
   getHero(id: number): Promise<Hero> {
-    // let hero = HEROES.find(nextHero => nextHero.id === id);
-    // if (hero) {
-    //   return Promise.resolve(hero);
-    // }
-    // return Promise.reject('Hero not found for id: ${id}');
     return Promise.resolve(HEROES)
       .then(heroes => heroes.find(hero => hero.id === id));
   }
