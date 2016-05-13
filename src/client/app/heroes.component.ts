4cdbaca343d66069d77a374a4ec81b8a7ac8509a
Michael Westphal <michael@michaelwestphal.org>
2016-05-13 07:11:58 -0500

    Updating HeroesComponent navigation.
    Now instead of loading the HeroDetailComponent within its template, the HeroesComponent, on selected Hero, displays a 'View Details' link that routes to the HeroDetailComponent instead.

--- src/client/app/heroes.component.ts
+++ src/client/app/heroes.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
 
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
+// import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @Component({
   moduleId: __moduleName,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: ['heroes.component.css'],
-  directives: [HeroDetailComponent]
+  styleUrls: ['heroes.component.css']
+  // directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private router: Router) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -27,5 +30,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', { id: this.selectedHero.id }];
+    this.router.navigate(link);
   }
 }
